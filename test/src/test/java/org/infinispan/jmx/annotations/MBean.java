7da1721791fffd4e0ae857d2dce43cd53360e6b1
Adrian Nistor <anistor@redhat.com>
2020-01-06 11:45:03 +0200

    Resync our dummy annotations with Infinispan: MBean, ManagedAttribute, ManagedOperation

--- test/src/test/java/org/infinispan/jmx/annotations/MBean.java
+++ test/src/test/java/org/infinispan/jmx/annotations/MBean.java
@@ -1,14 +1,19 @@
 package org.infinispan.jmx.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This is a copy of the real annotation found in Infinispan source tree. Should be kept in sync.
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 @Inherited
+@Documented
 public @interface MBean {
 
    String objectName() default "";
--- test/src/test/java/org/infinispan/jmx/annotations/ManagedAttribute.java
+++ test/src/test/java/org/infinispan/jmx/annotations/ManagedAttribute.java
@@ -1,12 +1,17 @@
 package org.infinispan.jmx.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This is a copy of the real annotation found in Infinispan source tree. Should be kept in sync.
+ */
 @Retention(RetentionPolicy.CLASS)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target(ElementType.METHOD)
+@Documented
 public @interface ManagedAttribute {
 
    String description() default "";
